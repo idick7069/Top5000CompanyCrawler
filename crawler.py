853b722555af5f65a3107f99bea4224b5a1d2f4a
FrankWu <M10623004@gemail.yuntech.edu.tw>
2019-03-08 18:11:37 +0800

    新增抓取產業地址、電話、產業別、董事長、總經理

--- crawler.py
+++ crawler.py
@@ -6,8 +6,8 @@ import pandas as pd
 #前置輸入
 
 #排名1~2000
-min_count = 1 
-max_count = 2000  
+min_count = 2562
+max_count = 5000  
 #帳號密碼
 username_edit = 'a123456'
 password_edit = '0000000'
@@ -24,8 +24,10 @@ username = driver.find_element_by_xpath("//input[@name='user']")
 password = driver.find_element_by_xpath("//input[@name='pass']")
 
 #帳號密碼
-username.send_keys(username_edit)
-password.send_keys(password_edit)
+# username.send_keys(username_edit)
+# password.send_keys(password_edit)
+username.send_keys('s2410231011')
+password.send_keys('l1224121')
 password.submit()
 
 
@@ -55,13 +57,16 @@ index_to.send_keys(str(max_count))
 #確定
 index_to.submit()
 
-time.sleep(10)
+time.sleep(5)
 
 #取得公司名稱 及 排名
 
 string_list =[]
 rank_list = []
+address_list = []
 
+winHandleBefore = driver.window_handles
+ 
 def getCompanyName():
     
     #  testString = driver.find_element_by_xpath("//html/body/table[1]/tbody/tr[4]/td/table/tbody/tr[2]/td[3]")
@@ -70,10 +75,42 @@ def getCompanyName():
        
         companyRank = driver.find_element_by_xpath('//html/body/table[1]/tbody/tr[4]/td/table/tbody/tr['+str(i)+']/td[2]')
         companyNameString = driver.find_element_by_xpath('//html/body/table[1]/tbody/tr[4]/td/table/tbody/tr['+str(i)+']/td[3]')
-        print(companyRank.text , companyNameString.text)
+        # print(companyRank.text , companyNameString.text)
+        companyRankText = companyRank.text
+        companyNameText = companyNameString.text
+
+        detail = driver.find_element_by_xpath('//html/body/table[1]/tbody/tr[4]/td/table/tbody/tr['+str(i)+']/td[4]/a')
+        detailWindow = detail.get_attribute("onclick");
+        detailWindowName = detailWindow.split(',')[1].replace('\'','')
+
+        detail.click()
+        #切換到彈出式視窗
+        driver.switch_to_window(detailWindowName)
+
+        time.sleep(1)
+        #彈出式視窗抓詳細
+        companyAddress = driver.find_element_by_xpath('//html/body/table[4]/tbody/tr/td/table/tbody/tr[4]/td[2]')
+        # companyType =  driver.find_element_by_xpath('//html/body/table[11]/tbody/tr[1]/td/font[2]/b/span')
+        companyPhone =  driver.find_element_by_xpath('//html/body/table[4]/tbody/tr/td/table/tbody/tr[5]/td[2]')
+        companyChairman = driver.find_element_by_xpath('//html/body/table[4]/tbody/tr/td/table/tbody/tr[9]/td[2]')
+        companyManager = driver.find_element_by_xpath('//html/body/table[4]/tbody/tr/td/table/tbody/tr[10]/td[2]')
+
         
+        try:
+                companyType =  driver.find_element_by_xpath('//html/body/table[11]/tbody/tr[1]/td/font[2]/b/span')
+        except:
+               companyType = '搜尋有誤請手動更新'
+        
+        print(companyRankText , companyNameText , companyAddress.text , companyType.text , companyPhone.text , companyChairman.text , companyManager.text)
+
+        driver.close()
+        driver.switch_to_window('')
+        # driver.switch_to_default_content()
+
+
         rank_list.append(rank_list)
         string_list.append(companyNameString.text)
+        address_list.append(companyAddress)
     
 getCompanyName()
 
@@ -82,7 +119,6 @@ next_btn = driver.find_element_by_xpath("//input[@name='submit1' and @value='下
 
 
 for i in range(1,int(max_count/5)):
-    print("------")
     next_btn = driver.find_element_by_xpath("//input[@name='submit1' and @value='下一頁']")
     next_btn.click()
     time.sleep(1)
